bdba4a4e8911c529e9c6727f5a27d2c50d97b2c2
Alfred <alfred@codenaturally.com>
2018-10-20 20:16:47 -0700

    profiles model added

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -18,5 +18,133 @@ interface IUser extends mongoose.Document {
 
 export const User = mongoose.model<IUser>('users', UserSchema);
 
+interface IProfile extends mongoose.Document {
+    user: string;
+    company?: string;
+    website?: string;
+    location?: string;
+    status?: string;
+    skills: string[];
+    bio?: string;
+    githubUsername?: string;
+    experience: Array<{
+        title: string;
+        company: string;
+        location?: string;
+        from: Date;
+        to?: Date;
+        description?: string;
+    }>;
+    education: Array<{
+        school: string;
+        degree: string;
+        field: string;
+        from: Date;
+        to?: Date;
+        description?: string;
+    }>;
+}
+
 export const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: true,
+    },
+    company: {
+        type: String,
+    },
+    website: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    status: {
+        type: String,
+    },
+    skills: {
+        type: [String],
+        required: true,
+    },
+    bio: {
+        type: String,
+    },
+    githubUsername: {
+        type: String,
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true,
+            },
+            company: {
+                type: String,
+                required: true,
+            },
+            location: {
+                type: String,
+            },
+            from: {
+                type: Date,
+                required: true,
+            },
+            to: {
+                type: Date,
+            },
+            description: {
+                type: String,
+            },
+        },
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true,
+            },
+            degree: {
+                type: String,
+                required: true,
+            },
+            field: {
+                type: String,
+                required: true,
+            },
+            from: {
+                type: Date,
+                required: true,
+            },
+            to: {
+                type: Date,
+            },
+            description: {
+                type: String,
+            },
+        },
+    ],
+    social: {
+        youtube: {
+            type: String,
+        },
+        twitter: {
+            type: String,
+        },
+        facebook: {
+            type: String,
+        },
+        linkedin: {
+            type: String,
+        },
+        instagram: {
+            type: String,
+        },
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    }
 });
+
+export const Profile = mongoose.model<IProfile>('profiles', ProfileSchema);
